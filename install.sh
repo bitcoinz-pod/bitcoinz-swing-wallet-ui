21a6099a9d24213ace25da1314db507c2d33d1ec
ezzygarmyz <dragkhai@gmail.com>
2024-12-27 01:18:38 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+# Note: 
+# Ensure that the BitcoinZ node is fully synchronized before running the BitcoinZ Wallet GUI
+
 sudo apt update -y
 
 sudo apt install -y default-jdk openjdk-11-jdk
 
 wget https://github.com/btcz/bitcoinz-wallet/releases/download/v2.0.10/bitcoinz-wallet_2.0.10.deb
 
-sudo dpkg -i bitcoinz-wallet_2.0.10.deb
+dpkg-deb -x bitcoinz-wallet_2.0.10.deb ./bitcoinz-temp
+#here we extract .jar files from the package (no need to install it)
+
+sudo mkdir /usr/lib/bitcoinz-wallet
+sudo mv ./bitcoinz-temp/usr/lib/bitcoinz-wallet/* /usr/lib/bitcoinz-wallet/
 
 rm -f bitcoinz-wallet_2.0.10.deb
+rm -r ./bitcoinz-temp
 
 wget https://github.com/btcz/bitcoinz/releases/download/2.1.0/bitcoinz-c73d5cdb2b70-x86_64-linux-gnu.tar.gz
 
